728a72809674c8e1c79132d4ecdd404207e5edd3
orange22 <dmitriy.bozhok@gmail.com>
2014-08-21 19:24:33 +0300

    123

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
-
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from database import init_db
 
 app = Flask(__name__)
+Bootstrap(app)
+app.config.from_object('config')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+init_db()
 
-if __name__ == "__main__":
-    app.run()
+
+#db = SQLAlchemy(app)
+
+from app import views, models
